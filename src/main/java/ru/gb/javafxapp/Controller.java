495f431a25dc787eee896ac2347e61a2ab561f9e
Alex <252459@gmail.com>
2022-06-12 14:47:53 +0300

    homework

--- src/main/java/ru/gb/javafxapp/Controller.java
+++ src/main/java/ru/gb/javafxapp/Controller.java
@@ -12,48 +12,17 @@ public class Controller {
     private TextArea historyArea;
     @FXML
     private TextField userAnswer;
-    private Game game;
-    private int step;
-
+    private MeChat meChat;
     public Controller() {
-        this.game = new Game();
+        MeChat meChat = new MeChat();
     }
+
 
     public void clickCheckButton(ActionEvent actionEvent) {
         String answer = userAnswer.getText();
-        if (answer.isBlank()) {
-            return;
-        }
-        Game.BullAndCowApplication count = game.calculateBullsAndCow(answer);
-        String text = String.format("%d. Введено число %s, количество быков %d, количество коров %d", ++step, answer, count.getBulls(), count.getCows());
-        historyArea.appendText(text + "\n");
+        historyArea.appendText(answer + "\n");
         userAnswer.clear();
         userAnswer.requestFocus();
-
-        if (count.getBulls() == 4) {
-            if (ifWantToPlayAgain()) {
-                clickNewGame();
-            } else {
-                System.exit(0);
-            }
-        }
-    }
-
-    private boolean ifWantToPlayAgain() {
-        final Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
-                "Поздравляю, Вы выиграли\n" + "Загаданное число " + game.getGuessNum() + ".\n\n" + "Желаете сыграть ещё?",
-                new ButtonType("Да", ButtonBar.ButtonData.YES),
-                new ButtonType("Нет", ButtonBar.ButtonData.NO)
-        );
-        alert.setTitle("Поздравляю!");
-        final ButtonType answer = alert.showAndWait().get();
-        return answer.getButtonData() == ButtonBar.ButtonData.YES;
-    }
-
-    public void clickNewGame() {
-        step = 0;
-        historyArea.appendText("\n\n ---Start new game---\n\n");
-        this.game = new Game();
     }
 
     public void Exit() {
--- src/main/java/ru/gb/javafxapp/MeChatApplication.java
+++ src/main/java/ru/gb/javafxapp/MeChatApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class BullAndCowApplication extends Application {
+public class MeChatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BullAndCowApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MeChatApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Bulls And Cow Game!");
+        stage.setTitle("MeChat");
         stage.setScene(scene);
         stage.show();
     }
